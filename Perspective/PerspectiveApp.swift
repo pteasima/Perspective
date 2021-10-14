738dee842192c5221fac144ce95a39cf810d757c
Petr Šíma <ptea.sima@gmail.com>
2021-10-14 12:46:01 +0200

    Initial Commit

---
+++ Perspective/PerspectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerspectiveApp.swift
+//  Perspective
+//
+//  Created by Petr Šíma on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct PerspectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
